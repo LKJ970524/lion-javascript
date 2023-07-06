4ce7e288c2688fa45c5c641b9d06aa459b43ec7c
LKJ970524 <dlrbwjd7096@naver.com>
2023-07-06 10:26:47 +0900

    07.05, 07.06수업

---
+++ client/chapter/core/08-1.condition.js
@@ -0,0 +1,106 @@
+/* ---------------- */
+/* Condition        */
+/* ---------------- */
+
+/*
+
+if("ㅁ"){
+  console.log('이 조건은 항상 실행됩니다.');
+}else {
+  console.log('조건에 부합하지 않습니다.');
+}
+
+*/
+
+/*
+let result = prompt('자바스크립트의 공식 이름은 무엇일까요','').toLowerCase();
+
+
+if(result === 'ecmascript'){
+  console.log('정답입니다!');
+}else {
+  console.log('모르셨나요?');
+}
+
+*/
+
+/*
+let value = prompt('숫자를 입력하세요.','숫자');
+
+if(value > 0){
+  console.log(1);
+}else if(value < 0){
+  console.log(-1);
+}else{
+  console.log(0);
+}
+
+*/
+
+
+// let result = ( a + b < 4 ) ? '미만' : '이상';
+
+
+// 그 영화 봤니?
+//     ↓
+// Yes | No
+//     | 영화 볼거니?
+//           ↓
+//       Yes | No
+
+// 영화 봤니?
+let didWatchMovie = confirm('너 엘리멘탈 영화 봤니?','');
+
+// 영화 볼거니?
+let goingToWatchMovie = 'yes';
+
+
+if(!didWatchMovie){
+
+  //영화 볼거니?
+  let goingToWatchMovie = confirm('영화 볼거니?');
+
+  if(goingToWatchMovie){
+    let withWho = prompt('누구랑 볼거니','')
+
+    if(withWho === '여자친구'){
+      console.log('ㅋㅋㅋㅋㅋㅋㅋㅋㅋ여친이랰ㅋㅋ')
+    }else if(withWho === '가족'){
+      console.log('화목하네');
+    }else{
+      console.log('재밌게보구와~~~~');
+    }
+  }
+}else {
+  let alone = confirm('너 혼자 봤니?');
+
+  if(alone){
+    let didCry = confirm('너 울었니..?');
+
+    if (didCry){
+      console.log('너 찌질했네..?');
+    }else{
+      console.log('너 C야 T발롬아?');
+    }
+  }
+}
+
+
+
+// if 문(statement)
+// if(didWatchMovie === 'yes'){
+//   console.log('무슨 영화 봤어?');
+
+// }else if (goingToWatchMovie === 'yes') {
+//   console.log('나랑 영화 보러 갈래?');
+// }else{
+//   console.log('그래...');
+// }
+
+// else 절(caluse)
+
+// else if 복수 조건 처리
+
+// 조건부 연산자
+
+// 멀티 조건부 연산자 식
