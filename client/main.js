a1827da8c903dbd872ee7f57dfb4b905a5417504
LKJ970524 <dlrbwjd7096@naver.com>
2023-07-13 16:08:40 +0900

    07-13 수업자료

--- client/main.js
+++ client/main.js
@@ -1,3 +1,19 @@
-// console.log('hello javascript!')
+// console.log('hello js!');
+// confirm('정말 지울꺼야..?');
 
-// alert('경고!');
+
+
+
+const input = document.querySelector('#todo');
+
+let value = input.value;
+
+input.addEventListener('input',()=>{
+  value = input.value;
+  console.log(value === 'hello');
+})
+
+
+
+// input.classList.add('is-active');
+// input.classList.remove('is-active');
