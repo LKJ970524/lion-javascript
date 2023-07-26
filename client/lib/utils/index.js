142aaa56046f3a24d85154ce339f942eda730713
LKJ970524 <dlrbwjd7096@naver.com>
2023-07-26 23:25:18 +0900

    16일차 수업

--- client/lib/utils/index.js
+++ client/lib/utils/index.js
@@ -6,8 +6,9 @@
 
 // re-export
 
+export * from './xhr.js';
 export * from './copy.js';
 export * from './memo.js';
+export * from './delay.js';
+export * from './tiger.js';
 export * from './typeOf.js';
-export * from './xhr.js';
-
--- client/main.js
+++ client/main.js
@@ -1,1 +1,5 @@
-import {} from './lib/index.js';
+// fetch 는 왜 쓰는 건가요?
+
+import { insertLast, tiger } from './lib/index.js';
+
+const URL = 'https://jsonplaceholder.typicode.com/users/1';
